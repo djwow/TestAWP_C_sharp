7e0e1355239ede691a613bf0973d829a5b3a5828
djwow <vorekhov88@gmail.com>
2017-06-28 11:31:33 +0300

    1

---
+++ ClassLibrary1/ClassLibrary1/Main.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ClassLibrary1
+{
+    public class Main
+    {
+        IWebDriver driver = new ChromeDriver();
+        [Test]
+        public void myFirstTest()
+        {
+            driver.Navigate().GoToUrl("http://www.google.com");
+            Assert.AreEqual("Google", driver.Title);
+            driver.Close();
+            driver.Quit();
+        }
+
+    }
+}
